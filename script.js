84e9ef68abdd51192bf5936fde1f6fac97c79818
abishejk1872 <abisheknewar15@gmail.com>
2025-01-10 14:22:05 +0530

    dom introduction

---
+++ script.js
@@ -0,0 +1,9 @@
+let header = document.getElementById("header")
+header.style.color = "red"
+header.textContent = "CSS"
+header.style.backgroundColor = "yellowgreen"
+// header.innerHTML = "<div>INNER DIV</div>"
+
+var a =10;
+
+header.innerHTML = `VALUE = ${a}`
