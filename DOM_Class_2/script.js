7f2dc59e8321b7ee5672ceb0dcb12d7fc67eeb7f
abishejk1872 <abisheknewar15@gmail.com>
2025-01-13 13:48:01 +0530

    dom class 2

---
+++ DOM_Class_2/script.js
@@ -0,0 +1,18 @@
+// var container = document.getElementById("container")
+// console.log(container)
+// console.log(container.innerHTML)
+// console.log(container.innerText)
+// console.log(container.textContent)
+// container.style.backgroundColor = "red"
+// container.style.color = "white"
+
+// var container = document.getElementsByClassName("container")
+// console.log(container[1].innerHTML)
+
+// var container = document.getElementsByTagName("div")
+// console.log(container[0].innerText)
+
+// var container = document.querySelector("div")
+// console.log(container)
+// container.innerText ="TRIAL"
+
