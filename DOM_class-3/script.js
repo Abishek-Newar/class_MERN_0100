089f3e98118ab4385479bd1edda9398378282aa0
abishejk1872 <abisheknewar15@gmail.com>
2025-01-14 14:01:54 +0530

    changes

---
+++ DOM_class-3/script.js
@@ -0,0 +1,26 @@
+// var container = document.getElementById("container")
+// const box = document.createElement("div")
+// box.innerText = "CSS"
+// box.style.border= "2px solid #000"
+// box.style.backgroundColor= "yellowgreen"
+// // container.append(box)
+// // container.prepend(box)
+
+// // container.before(box)
+// function addBox(){
+//     container.after(box)
+// }
+
+// var valueINput = document.getElementById("input").value
+
+
+function print(a,b){
+    console.log(a+b)
+}
+
+print(10,20)
+
+var sum = () =>{
+    console.log("hello")
+}
+sum()
