5c577a5480f76675cfc78cccdc35a1479a78002c
abishejk1872 <abisheknewar15@gmail.com>
2025-01-17 17:28:33 +0530

    changes

---
+++ 5_class_01/script.js
@@ -0,0 +1,41 @@
+console.log("10")
+
+var a = "hello"
+console.log(a)
+const c =30
+const b = 20;
+
+
+console.log(c+b)
+var d =10;
+d+=1
+console.log(d)
+
+console.log(c>d)
+
+console.log(10==="10")
+
+console.log(b<c && c>d)
+
+console.log(b>c || c>d)
+
+console.log(!(b<c))
+
+
+if(b<c){
+    console.log("if executed")
+}else{
+    console.log("else executed")
+}
+
+
+if(b>c && b>d){
+    console.log("b is greatest")
+}else if(c>b && c>d){
+    console.log("c is greatest")
+}else{
+    console.log("d is greatest")
+}
+
+// take a number and print if it is even or odd 
+// take five numbers and print the greatest out of it
