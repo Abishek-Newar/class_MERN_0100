d1772e31a16d8ab0ddb49e2023f04986f96de282
abishejk1872 <abisheknewar15@gmail.com>
2025-01-23 13:53:06 +0530

    async await and fetch

--- promises/script.js
+++ promises/script.js
@@ -1,28 +1,32 @@
 const p1 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject("hello 1")
-    },2000)
+    resolve("hello 1")
 })
-console.log(p1)
-// p1.then((msg)=>{console.log("resolved",msg)}).catch((msg)=>{
-//     console.log("rejected",msg)
-// })
 
-
+p1.then((msg)=>{
+    console.log("resolved",msg)
+}).catch((error)=>{
+    console.log("rejected",error)
+})
 const p2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject("hello 2")
-    },2000)
+    resolve("hello 2")
 })
-
 const p3 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject("hello 3")
-    },2000)
+    resolve("hello 3")
 })
 
 Promise.all([p1,p2,p3]).then((msg)=>{
-    console.log(msg)
-}).catch((msg)=>{
-    console.log("rejected",msg)
-})
+        console.log("resolved",msg)
+    }).catch((error)=>{
+        console.log("rejected",error)
+    })
+    
+fetch('https://dummyjson.com/products') 
+.then((res)=>res.json())
+.then((data)=>console.log(data))   
+
+async function serverCall(){
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    console.log("fucntion",data)
+}
+serverCall()
