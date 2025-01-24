2effff8a2365699f7da40eff58610241af9f00b1
abishejk1872 <abisheknewar15@gmail.com>
2025-01-24 17:28:59 +0530

    changes

---
+++ 5_loops/script.js
@@ -0,0 +1,42 @@
+for(let i = 0;i<10;i++){
+    console.log(i)
+}
+
+
+let j = 0;
+while(j<10){
+    console.log(j)
+    j+=1
+}
+
+do{
+    console.log("hello")
+}while(false);
+
+var arr = [21,32,12,23]
+
+arr.forEach((item)=>{
+    console.log(item)
+})
+
+for(var b of arr){
+    console.log(b)
+}
+
+for (var itm in arr){
+    console.log(arr[itm])
+}
+
+// print all the prime numbers in range 0-50
+
+// *
+// **
+// ***
+// ****
+// *****
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********      
