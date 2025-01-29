5eb2f1020fe404edba9a5360095c228f9d8112e3
abishejk1872 <abisheknewar15@gmail.com>
2025-01-29 17:30:30 +0530

    changes

--- 5_DOM_manupulation/script.js
+++ 5_DOM_manupulation/script.js
@@ -1,6 +1,6 @@
 var container = document.getElementById("container")
 var con1 = document.getElementsByClassName("container")
-console.log(con1[1].innerHTML)
+console.log(con1[0].innerHTML)
 var con2 = document.getElementsByTagName("div")
 console.log(con2[0].innerHTML)
 var con3 = document.querySelector(".container")
@@ -11,3 +11,26 @@ console.log(container.innerText)
 console.log(container.textContent)
 
 container.style.color = "yellowgreen"
+
+var box = document.createElement("div")
+box.textContent = "hello there"
+
+container.before(box)
+// after append prepend
+
+const cls = container.classList
+
+container.classList.add("hello")
+container.classList.remove("con1")
+console.log(cls)
+
+var atr = container.getAttribute("id")
+container.setAttribute("name","hello")
+console.log(atr)
+
+const data = document.createElement("button")
+data.innerText = "click me"
+data.onclick = ()=>{
+    alert("function called")
+}
+container.append(data)
