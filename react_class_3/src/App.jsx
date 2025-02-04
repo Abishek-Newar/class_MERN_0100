0ddeb33c50094bc63713bb9b5e640e8b5b52bae5
abishejk1872 <abisheknewar15@gmail.com>
2025-02-04 13:59:35 +0530

    changes

---
+++ react_class_3/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import Demo from "./components/Demo"
+
+
+function App() {
+  const [count,setCount] = useState(0)
+  const [name,setName] = useState("abishek")
+
+  function increment(){
+    setCount(count  +1)
+    setName("hello")
+    console.log(count)
+  }
+
+  function decrement(){
+    setCount(count - 1)
+    setName("hii")
+    console.log(count)
+  }
+  return (
+    <>
+    <button onClick={increment}>inc</button>
+    <h1>{count}</h1>
+    <p>{name}</p>
+    <button onClick={decrement}>dec</button>      
+    </>
+  )
+}
+
+export default App
