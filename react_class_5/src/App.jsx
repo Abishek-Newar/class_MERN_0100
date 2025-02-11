e4be50582cb10628f26a31e1d68d87525dc38afb
abishejk1872 <abisheknewar15@gmail.com>
2025-02-11 14:38:09 +0530

    changes

--- react_class_5/src/App.jsx
+++ react_class_5/src/App.jsx
@@ -1,11 +1,16 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 
-
+const nums = new Array(30_000_000).fill(0).map((_,index)=>{
+  return {
+    index: index,
+    isMagical: index===29_000_000
+  }
+})
 function App() {
   //hooks: to perform a certain operations 
   // 1. useState: to make state mutable 
   // 2. useEffect : to stop from unnecessary rerendering
-  // 3. useRef: to select the DOM element 
+  // 3. useRef: to select the DOM element / fixing values over re renders
   // 4. useMemo 
   // 5. useCallback
   let [data,setData] = useState("hello")
@@ -16,23 +21,29 @@ function App() {
     setData("hii")
     console.log(data)
   }
-
+  let a = useRef(0)
   function handleCount(){
+    a.current+=1
+    console.log(a.current)
     setCount(count+1)
   }
 
   function handleTable(){
     setTable(table + 7)
-    ref.current.style.backgroundColor = "red"
-    ref.current.style.color = "white"
+    buttonRef.current.style.backgroundColor = "yellowgreen"
   }
 
+  const [num,setNum] = useState(nums)
+
+  const magical = useMemo(()=>{
+    return num.find((item)=>item.isMagical === true)
+  },[])
 
   useEffect(()=>{
     console.log("re rending...")
   },[table])
 
-  let ref = useRef()
+  let buttonRef = useRef()
 
 
   // let value = 10 
@@ -42,12 +53,13 @@ function App() {
   // }
   return (
     <>
+    {magical.index}
       {data}
       <button onClick={handleCall}>call</button>
       <br />
       <button onClick={handleCount}>{count}</button>
       <br/>
-      <button ref={ref} onClick={handleTable}>{table}</button>
+      <button ref={buttonRef}  onClick={handleTable}>{table}</button>
       {/* {value}
       <button onClick={changeValue}>change</button> */}
     </>
